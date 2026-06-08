188df6e95829a77c4608ec6792ed0132dbbe7196
VigneshSQA <vigneshbca.56@gmail.com>
2026-06-08 18:54:13 +0530

    Update Test_From_Local_To_Git_As_New_File.java
    Just added ! at the end of print statement

--- src/test/java/testpackage/Test_From_Local_To_Git_As_New_File.java
+++ src/test/java/testpackage/Test_From_Local_To_Git_As_New_File.java
@@ -4,7 +4,7 @@ public class Test_From_Local_To_Git_As_New_File {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Test to push new code change from Local to GitHub");
+		System.out.println("Test to push new code change from Local to GitHub!");
 
 	}
 
